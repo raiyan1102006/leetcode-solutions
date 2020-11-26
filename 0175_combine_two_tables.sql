c005a5a08b6749a4c687f29530b59bb662458fb4
Raiyan Abdul Baten <raiyanabdulbaten@gmail.com>
2020-11-26 16:33:40 -0500

    update

---
+++ 0175_combine_two_tables.sql
@@ -0,0 +1,8 @@
+-- 175. Combine Two Tables
+-- https://leetcode.com/problems/combine-two-tables/
+
+
+-- # Write your MySQL query statement below
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+FROM Person LEFT JOIN Address
+ON Person.PersonID=Address.PersonID;
